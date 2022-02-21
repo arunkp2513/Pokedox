40c6526db5d63e9463372ef9a1e435ec72ae1fa5
arunkp2513 <99520584+arunkp2513@users.noreply.github.com>
2022-02-20 21:32:46 -0800

    test

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,3 @@ let myName = 'Arun';
 document.write(myName);
 let myName = 'Apoorva';
 document.write(myName);
-
-let myFavouriteFood = 'dosa';
-document.write('myFavouriteFood');
-
-alert('Hello world');
