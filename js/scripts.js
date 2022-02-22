e41c7cff46d36fa269d20d465a21b3215746a29e
arunkp2513 <99520584+arunkp2513@users.noreply.github.com>
2022-02-21 21:17:20 -0800

    new js array

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-let pokemon1 = {
-  name: 'Bulbasur'
-  height: 7
-  type: ['grass', 'poison']
-};
-let pokemon2 = {
-  name: 'Incineroar'
-  height: 6
-  type: ['fire',  'dark' ]
-};
-let pokemon3 = {
-  name: 'Pidgey'
-  height: 1
-  type: ['normal', 'flying']
-};
-
-let pokemonlist = [pokemon1, pokemon2, pokemon3];
+let pokemonList = [
+  {
+    name: "Bulbasur",
+    height: 7,
+    type: ["grass", "poison"],
+  },
+  {
+    name: "Incineroar",
+    height: 6,
+    type: ["fire", "dark"],
+  },
+  {
+    name: "Pidgey",
+    height: 1,
+    type: ["normal", "flying"],
+  },
+];
