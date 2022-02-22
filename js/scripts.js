e6c811515cb0f14cc7253a9b779b292f8a3bfb35
arunkp2513 <99520584+arunkp2513@users.noreply.github.com>
2022-02-21 17:45:17 -0800

    new js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('hello world');
+let pokemon1 = {
+  name: 'Bulbasur'
+  height: 7
+  type: ['grass', 'poison']
+};
+let pokemon2 = {
+  name: 'Incineroar'
+  height: 6
+  type: ['fire',  'dark' ]
+};
+let pokemon3 = {
+  name: 'Pidgey'
+  height: 1
+  type: ['normal', 'flying']
+};
 
-let myName = 'arun';
-document.write(myName);
+let pokemonlist = [pokemon1, pokemon2, pokemon3];
